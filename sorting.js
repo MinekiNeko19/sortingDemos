f9e873ee5c5de2a8724f2bdb2cd55fe6fc798b77
Sarah Leong <sleong20@stuy.edu>
2023-07-02 00:34:27 -0400

    generates a random array

--- sorting.js
+++ sorting.js
@@ -1,1 +1,27 @@
+const unsortedArr = []
+let sortedArr = [];
 
+function randomizeArray(len,min=-500,max=500) {
+  // will only fill array with ints from [-min,max]
+  if (max < min) {
+    const temp = max;
+    max = min;
+    min = temp;
+  }
+
+  unsortedArr.splice();
+  for (let i = 0; i < len; i++) {
+    const n = Math.floor(Math.random()*(max-min+1)) + min;
+    unsortedArr.push(n);
+  }
+  
+  console.log(unsortedArr);
+  document.getElementById('originalArr').innerHTML = 'Generated Random Array: [' + unsortedArr.toString() + ']';
+}
+
+function selectionSort() {
+  sortedArr = unsortedArr.copyWithin();
+
+  document.getElementById('sortedArr').innerHTML = 'Sorted Using Selection Sort: [' + sortedArr.toString() + ']';
+
+}
